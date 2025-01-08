34d01513ac830cda94537d674b67dfb3265581da
jose rodriguez <joserockabarcelona@gmail.com>
2025-01-08 18:14:15 +0100

    restructured code

--- loops/loops-arrays/ej1.mjs
+++ loops/loops-arrays/ej1.mjs
@@ -2,7 +2,6 @@
 
 // 1. Create a function which receives the imported data as parameter and
 //    returns an array of only the titles
-
 function jsonTitles(data) {
   let titles = [];
   for (let i = 0; i < data.length; i++) {
