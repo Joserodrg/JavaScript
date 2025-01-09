362b81491c37f238e77c8e7f02c9656b120ab67d
jose rodriguez <joserockabarcelona@gmail.com>
2025-01-09 17:24:54 +0100

    added async theory, exemples

---
+++ async/callback.js
@@ -0,0 +1,14 @@
+// Declarar función
+function obtenerDatos(callback) {
+    setTimeout(function(){
+      const datos = {nombre: "John", edad: 30};
+      callback(datos);
+    }, 3000);
+  }
+  
+  // Ejecutar función con una callback
+  obtenerDatos(function(datos) {
+    console.log(datos);
+  });
+  
+  console.log("Se están obteniendo los datos...");s
---
+++ async/exemple.js
@@ -0,0 +1,6 @@
+console.log("Iniciando...");
+
+setTimeout(function() {
+  console.log("descarga completada");
+}, 2000);
+
